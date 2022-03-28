08bb0f344275b8db25864baae169b02ceb9a70e4
CallMeBill <callmebill11@outlook.com>
2022-03-28 21:09:13 +0800

    220328

---
+++ Codes/8-3_Fit.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+from sklearn import svm
+import xgboost as XGB
+from sklearn.ensemble import RandomForestClassifier as RFC
+import Codes.fit_fc as fc
+
+path = "E:/wkh/Codes/Projects/TC_miRNA"
+# 读取文件
+s_count = pd.read_csv(path + '/Data/s_count.csv')
+s_rpm = pd.read_csv(path + '/Data/s_rpm.csv')
+with open(path + '/Data/degs.txt', 'r') as f:
+    degs = []
+    for line in f:
+        degs.append(line.strip())
+with open(path + '/Data/degs.sub.txt', 'r') as f:
+    sub = []
+    for line in f:
+        sub.append(line.strip())
+SampleGroup = pd.read_csv(path + '/Data/SampleGroup.csv')
+# 数据预处理
+SampleGroup.iloc[SampleGroup.iloc[:, 1] == 'cancer', 1] = '1'
+SampleGroup.iloc[SampleGroup.iloc[:, 1] == 'normal', 1] = '0'
+SampleGroup.index = SampleGroup.iloc[:, 0]
+SampleGroup.drop('EntitiesId', axis=1, inplace=True)
+
+rpm_T = s_rpm.T
+rpm_T.columns = rpm_T.iloc[0]
+rpm_T.drop('NAME', inplace=True)
+rpm1 = rpm_T.loc[:, degs]
+
+X_rfe = np.array(rpm1)
+y_rfe = np.array(SampleGroup)
+
+clf = svm.SVC(kernel='linear', C=1, probability=True)
+fc.fit_c(clf, X_rfe, y_rfe, 5)
+
+rfc = RFC(n_estimators=81, max_depth=20)
+fc.fit_c(rfc, X_rfe, y_rfe, 5)
+
+xgb = XGB.XGBClassifier(learning_rate=0.1,
+                        n_estimators=81,
+                        max_depth=3,
+                        min_child_weight=1,
+                        gamma=0.0,
+                        colsample_bytree=0.8,
+                        subsample=0.8,
+                        reg_alpha=1e-05,
+                        reg_lambda=1)
+fc.fit_c(xgb, X_rfe, y_rfe, 5)
